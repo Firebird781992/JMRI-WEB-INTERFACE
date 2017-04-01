996b0202e02ba19b1271b8a2f202cada76fee2c2
Jason <Firebird781992@gmail.com>
2017-04-01 14:50:06 -0400

    corrected ip
    corrected ip

--- src/roster.js
+++ src/roster.js
@@ -1,9 +1,9 @@
 var request = require('request');
 
 function getRoster(callback) {
-  request('http://192.168.1.112:12080/json/roster', function (error, response, body) {
+  request('http://67.253.220.188:12080/json/roster', function (error, response, body) {
       body = JSON.parse(body)
-      callback(error,body) 
+      callback(error,body)
           });
 }
 
